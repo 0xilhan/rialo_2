71e3765099643d7733aadadfe4804d18ef664ba7
ilhan <mail.ilhanx29@gmail.com>
2025-10-01 22:58:21 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <main style={{
+      display: "flex",
+      height: "100vh",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "black",
+      color: "white",
+      flexDirection: "column"
+    }}>
+      <h1 style={{ fontSize: "2rem" }}>Rialo Quiz dApp</h1>
+      <p>Welcome! Quiz + SBT integration coming soon.</p>
+    </main>
+  )
+}
