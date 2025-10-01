1a596db007ea666fc2b347053b90eba99f5cd6cd
ilhan <mail.ilhanx29@gmail.com>
2025-10-01 23:00:20 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: { appDir: false }
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
+
